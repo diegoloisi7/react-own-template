8dc09b171ae6677538d4981d700c650a87599939
diegoloisi7 <diego.-loisi@hotmail.com>
2025-01-24 15:33:35 -0300

    v1.1 fix pages folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import Landing from "./pages/Landing.tsx"
+import { Landing } from "@pages/index"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
